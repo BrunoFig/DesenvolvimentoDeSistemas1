e21511f985fa1f6ff8a5b4badf598c841e32a0a8
BrunoFig <brun094_figueiredo@hotmail.com>
2018-12-07 21:47:30 -0200

    Adicionado permissões de requisições HTTP

--- api/server.js
+++ api/server.js
@@ -112,6 +112,13 @@ var reservationsSchema = new Schema({
 //===========================================================
 //===========================================================
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 //                         GET Resquests
 //===========================================================
 app.get('/hosts', (req, res) =>{
